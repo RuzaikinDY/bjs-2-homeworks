f2bdbd7e686b0f8885921de4f58fe8db558089ab
RuzaikinDY <Devilwind@mail.ru>
2022-09-19 00:19:09 +0300

    add hw base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,17 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let discriminant = Math.pow(b, 2) - 4 * a * c; //формула рассчет дискриминанта
+  
+  if (discriminant > 0) {
+    let meaning1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let meaning2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(meaning1,meaning2)
+  }
+  else if (discriminant === 0){
+    let meaning1 = -b / (2 * a);
+    arr.push(meaning1);
+  }
   // код для задачи №1 писать здесь
   return arr; // array
 }
