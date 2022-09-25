40f5d881c38d240c0f6f6c2225e8ffac8a27f0ea
RuzaikinDY <Devilwind@mail.ru>
2022-09-25 08:14:04 +0300

    add hw 2.function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,43 @@
-// Задание 1
+"use strict";
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max)
+      max = arr[i];
+    if (arr[i] < min)
+      min = arr[i];
+    sum = arr[i] + sum;
+  }
+  avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const calculatingElements = func(arrOfArr[i]);
+    if (calculatingElements > max) {
+      max = calculatingElements;
+    }
+  }
 
-  // Ваш кода
-  // for ...
-  
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let result = getArrayParams(arr);
+  return Math.abs(result.max - result.min);
 }
