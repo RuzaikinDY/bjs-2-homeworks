307d2f3040e51dd0779614fa8734fc3740ea27c6
RuzaikinDY <Devilwind@mail.ru>
2022-09-30 11:43:31 +0300

    add hw 3.arrays

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -15,10 +15,32 @@ function solveEquation(a, b, c) {
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//   debugger;
+//   const totalAmount = 0;
+//   const interestRate = percent / 12 / 100; //Процентная ставка. или /100/12
+//   const downPayment = contribution; //Сумма первоначального взноса.
+//   const loanАmount = amount; //Сумма кредита.
 
-  // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+//   if (interestRate < 0) {
+//     return `Параметр "Процентная ставка" содержит неправильное значение  ${percent}`;
+//   }
+//   else if (downPayment < 0) {
+//     return `Параметр начальный взнос содержит неправильное значение ${contribution}`;
+//   }
+//   else if (loanАmount < 0) {
+//     return `Параметр общая стоимость содержит неправильное значение ${amount}`;
+//   }
+//   else {
+//     const timeOfConclusion = new Date();
+//     const tern = date.getMonth() - timeOfConclusion.getMonth() + (12 * (date.getFullYear() - timeOfConclusion.getFullYear()));//Срок на который был выдан кредит(месяцах).
+//     //Метод getMonth Получить месяц, от 0 до 11.
+//     //Метод getFullYear Получить год (4 цифры).
+//     const loanBody = loanАmount - downPayment; //Тело кредита.
+//     const payment = loanBody * (interestRate + (interestRate / (((1 + interestRate) ** tern) - 1))); //Ежемесячная оплата.
+//     const totalSum = (payment * tern).toFixed(2);//Общая сумма, которую придется заплатить клиенту.
+//     const totalAmount = Number(totalSum);
+//     console.log("Общая сумма " + totalAmount);
+//     return totalAmount;
+//   }
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,32 @@
-function Student(name, gender, age) {
-    // Ваш код
+// function Student(name, gender, age) {
+//     // Ваш код
 
+// }
+
+// Student.prototype.setSubject = function (subjectName) {
+//   //ваш код
+// }
+
+// // ваш код для остальных методов
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+let student1 = new Student("Вася", "Мужской", 20);
+let student2 = new Student("Света", "Женский", 19);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = mark;
+  } else {
 
-// ваш код для остальных методов
+  }
+  Student.prototype.addMarks = function (...mark) {
+
+  }
+
+}
